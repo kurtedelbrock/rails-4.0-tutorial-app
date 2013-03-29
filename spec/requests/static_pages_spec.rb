620ebc28a399be4397d56a44a99aad6aa2c6428e
Kurt Edelbrock <kurt.edelbrock@lextech.com>
2013-03-29 00:00:20 -0400

    Added dynamic titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,29 @@
 require 'spec_helper'
 
-describe "Home page" do
-  it "User visits the 'Home Page'" do
-    visit '/static_pages/home'
-    expect(page).to have_content('Sample App')
-  end
-end
+describe "Static pages" do
 
-describe "Help page" do
-  it "should have the content 'Help'" do
-    visit '/static_pages/help'
-    expect(page).to have_content('Help')
-  end
-end
+  describe "Home page" do
 
-describe "About page" do
-  it "should have the content 'About Us'" do
-    visit '/static_pages/about'
-    expect(page).to have_content('About Us')
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
   end
 end
