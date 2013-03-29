aecbfccf8518feeb4d8e51fce433ce6a970c317a
Kurt Edelbrock <kurt.edelbrock@lextech.com>
2013-03-28 23:40:33 -0400

    Passing tests for the help page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
-describe "Static pages" do
-  describe "Home page" do
-    
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    
+describe "Home page" do
+  it "User visits the 'Home Page'" do
+    visit '/static_pages/home'
+    expect(page).to have_content('Sample App')
   end
 end
+
+describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
